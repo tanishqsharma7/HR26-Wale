a5bbfc9bad220f3af79ab7f3f79887abdfb28d35
Tanishq Sharma <90607852+tanishqsharma7@users.noreply.github.com>
2023-07-19 22:17:29 +0530

    third commit

--- App.js
+++ App.js
@@ -2,18 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element => Object => HTML Element (after rendering)
-
-// const heading =React.createElement("h1",
-// {id : "heading"},
-// "Namaste React");
-// console.log(heading);
-
-
-//JSX React - HTML like syntax
-//JSX code is transpiled before it reaches the JS engine by parcel(babel).
-//JSX => React.createelement => Reacr Element => Js object => HTML element
-
 
 const title = () => (
     <h1 className="head" tabIndex="5">
