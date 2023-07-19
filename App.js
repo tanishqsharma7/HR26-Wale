88a0a0e5de2928f6a2304d4138ee7b2ddf930785
Tanishq Sharma <90607852+tanishqsharma7@users.noreply.github.com>
2023-07-19 16:55:24 +0530

    second commit

--- App.js
+++ App.js
@@ -1,31 +1,27 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
 const parent = React.createElement(
     "div", 
     {id :"parent"},
     [React.createElement(
         "div",
         {id : "child"},
-        [React.createElement("h1",{},"I am an h1 tag") ,
+        [React.createElement("h1",{},"This is namaste react") ,
         React.createElement("h2",{},"I am an h2 tag")
     ]),
     React.createElement(
         "div",
         {id : "child2"},
-        [React.createElement("h1",{},"I am an h1 tag") ,
+        [React.createElement("h1",{},"I am an h1 tag.") ,
         React.createElement("h2",{},"I am an h2 tag")
     ])
 ]);
 
-//JSX can do this better than above written code
-
-
-
-// const heading = React.createElement(
-//     "h1" ,
-//     {id : "heading", xyz: "abc"},
-//     "Hello World from React Inside!"
-//     );
-
-// console.log(heading)        //object
+console.log(parent);  //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
