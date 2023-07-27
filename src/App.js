af3dc45184e0a5719036faa37295fb76eb7d4e7f
Tanishq Sharma <90607852+tanishqsharma7@users.noreply.github.com>
2023-07-27 15:12:25 +0530

    Sixth commit, Sorting top res

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Body from "./components/Body";
 
 
 
-
-
 const AppLayout = () => {
   return (
     <div className="app">
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,26 @@
 import RestrauntCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+
+  const[ListOfRestaurants, setListOfRestaurant]= useState(resList);
+
+
     return (
       <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+          <button className="filter-btn" 
+          onClick={()=> {
+            
+            const filteredList = ListOfRestaurants.filter(res=>res.info.avgRating >4.5);
+            setListOfRestaurant(filteredList);
+          }}>
+            Top Rated Restaurants</button>
+
+        </div>
         <div className="res-container">
-          {resList.map((restaurant) => (
+          {ListOfRestaurants.map((restaurant) => (
           <RestrauntCard key={restaurant.info.id} resData={restaurant} />
           ))}
         </div>
