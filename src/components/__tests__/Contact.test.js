a1a58928a6b2aef038cfc299d43900af604f7662
Tanishq Sharma <90607852+tanishqsharma7@users.noreply.github.com>
2023-08-24 16:23:58 +0530

    Logo added

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,38 +2,44 @@ import { render, screen } from "@testing-library/react"
 import Contact from "../Contact"
 import "@testing-library/jest-dom";
 
-test("Should load contact us component",()=>{
 
-    render(<Contact />);
+describe("Contact Us Page Test Cases",()=>{
+    test("Should load contact us component",()=>{
 
-    const heading = screen.getByRole("heading");
-    //Assertion
-    expect(heading).toBeInTheDocument();
-});
+        render(<Contact />);
 
-test("Should load button inside contact component", () => {
+        const heading = screen.getByRole("heading");
+        //Assertion
+        expect(heading).toBeInTheDocument();
+    });
 
-    render(<Contact />);
+    //it==test
 
-    const button = screen.getByText("Submit")
-    //Assertion
-    expect(button).toBeInTheDocument();
-});
+    it("Should load button inside contact component", () => {
 
-test("Should load input inside contact component", () => {
+        render(<Contact />);
 
-    render(<Contact />);
+        const button = screen.getByText("Submit")
+        //Assertion
+        expect(button).toBeInTheDocument();
+    });
 
-    const input = screen.getByPlaceholderText("Name");
-    //Assertion
-    expect(input).toBeInTheDocument();
-});
+    test("Should load input name inside contact component", () => {
 
-test("Should load 2 input boxes on the contact component",()=>{
-    render(<Contact />);
+        render(<Contact />);
 
-    const inputBoxes = screen.getAllByRole("textbox");
+        const input = screen.getByPlaceholderText("Name");
+        //Assertion
+        expect(input).toBeInTheDocument();
+    });
 
-    expect(inputBoxes.length).toBe(2);
+    test("Should load 2 input boxes on the contact component",()=>{
+        render(<Contact />);
+
+        const inputBoxes = screen.getAllByRole("textbox");
+
+        expect(inputBoxes.length).toBe(2);
+
+    });
 
 });
